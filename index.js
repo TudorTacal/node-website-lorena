35143f12fa95f2de44ad8b72d5c48e877cd93a39
Tacal <TACALTD@legal.regn.net>
2017-03-25 22:16:29 +0000

    Moves the site to a new design. To finish the about page and training page for MVP 1

--- index.js
+++ index.js
@@ -14,6 +14,10 @@ app.get('/', function(request, response) {
   response.render('index');
 });
 
+app.get('/trainings', function(request, response) {
+  response.render('trainings');
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
