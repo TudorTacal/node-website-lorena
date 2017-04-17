d1336a32e71cad50a38e2b89adae5dbf4243452e
Tacal <TACALTD@legal.regn.net>
2017-04-17 13:03:36 +0100

    Enables the form to send email to Lorena's address. WIP to hide the password in an env variable and investigate Google's less secure app policy. Plus display a success message once the email is sent or a failure message.

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 var express = require('express');
 var app = express();
+var nodemailer = require('nodemailer');
+var bodyParser = require('body-parser')
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+var jsonParser = bodyParser.json();
 
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
@@ -24,6 +33,37 @@ app.get('/nutrition', function(request, response){
 
 app.get('/contacts', function(request, response){
   response.render('contacts');
+});
+
+app.post('/contacts', jsonParser, function (req, res) {
+  var mailOpts, smtpTrans;
+
+  //Setup Nodemailer transport, I chose gmail. Create an application-specific password to avoid problems.
+  smtpTrans = nodemailer.createTransport({
+      service: 'Gmail',
+      auth: {
+          user: "lorenatacal@gmail.com",
+          pass: "Imortality1987!"
+      }
+  });
+
+  mailOpts = {
+      from: req.body.name + ' &lt;' + req.body.email + '&gt;',
+      to: 'lorenatacal@gmail.com',
+      subject: 'Mail from your Website',
+      text: req.body.message
+  };
+
+  smtpTrans.sendMail(mailOpts, function (error, response) {
+      if (error) {
+          console.log('Not sent');
+          res.render('contacts', { title: 'Lorena Personal Trainer - Contact', msg: 'Error occured, message not sent.', err: true, page: 'contacts' })
+      }
+      else {
+          console.log('sent');
+          res.render('contacts', { title: 'Lorena Personal Trainer - Contact', msg: 'Message sent! Thank you.', err: false, page: 'contacts' })
+      }
+  });
 });
 
 app.listen(app.get('port'), function() {
